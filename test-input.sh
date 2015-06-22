8ea80fba84ca3d63ba0fc0148e92b746c6723ad8
Tony Miller <tony.miller@eightyoptions.com>
2015-06-22 22:41:07 +1000

    more predictable test input pattern

--- test-input.sh
+++ test-input.sh
@@ -7,25 +7,37 @@ if [[ ! -p $pipe ]]; then
     exit 1
 fi
 
-#start at 60. nice reach
+#start at 0
 degrees=60
 side='L'
 speed=16
+sideDegrees=60
 
 while true; do
-  degrees=$(( RANDOM %= 180))
-  if (($degrees % 2 == 0 )) ;  then
-    side='R'
-  else
-    side='L'
+#  degrees=$(( RANDOM % 180))
+#  if (($degrees % 2 == 0 )) ;  then
+#    side='R'
+#  else
+#    side='L'
+#  fi
+
+  degrees=$(($degrees + 10));
+  if (($degrees >= 360)) ; then
+    degrees=$(($degrees - 360));
   fi
 
+  if (($degrees > 180)) ; then
+    sideDegrees=$((360 - $degrees));
+    side='R';
+  else
+    sideDegrees=$degrees
+    side='L';
+  fi
 
-
-  echo "\$IIVWR,$degrees,$side,$speed,N,01.2,M,04.4,K" > $pipe
+  echo "\$IIVWR,$sideDegrees,$side,$speed,N,01.2,M,04.4,K" > $pipe
   #echo "\$IIMWV,141.11,R,10,N,A" > $pipe
   # waterspeed
   waterspeed=10
   echo "\$IIVHW,,T,,M,$waterspeed,N,09.26,K" > $pipe
-  sleep 0.1
+  sleep 0.5
 done
